7c6958a3ef8ead0d641bb88c2b21827a4b801e47
Vraj2811 <121240839+Vraj2811@users.noreply.github.com>
2023-06-14 13:49:43 +0530

    TIcks added to To Do List

--- To Do List/1.js
+++ To Do List/1.js
@@ -1,39 +1,42 @@
 let i = 1;
 
 document.querySelector('#add').onclick = function () {
-    const idsContainer = document.querySelector('.ids');
-    const newDiv = document.createElement('div');
-    const checkboxId = `checkbox-${i}`;
-    const contentId = `content-${i}`;
-    const taskId = `task-${i}`;
+  const idsContainer = document.querySelector('.ids');
+  const newDiv = document.createElement('div');
+  const checkboxId = `checkbox-${i}`;
+  const contentId = `content-${i}`;
+  const taskId = `task-${i}`;
 
-    newDiv.className = 'content';
-    newDiv.id = contentId;
-    newDiv.innerHTML = `
+  newDiv.className = 'content';
+  newDiv.id = contentId;
+  newDiv.innerHTML = `
     <div class="checkbox" id="${checkboxId}">
+    &#10003;
     </div>
     <span class="task" id="${taskId}">
       ${document.querySelector('.chotacontainer').innerHTML}
     </span>
   `;
-    idsContainer.appendChild(newDiv);
+  idsContainer.appendChild(newDiv);
 
-    document.querySelector('.chotacontainer').innerHTML = "Enter the Task";
-    document.querySelector('.chotacontainer').style.color = "rgba(255, 255, 255, 0.472)";
-    i++;
+  document.querySelector('.chotacontainer').innerHTML = "Enter the Task";
+  document.querySelector('.chotacontainer').style.color = "rgba(255, 255, 255, 0.472)";
+  i++;
 }
 
 document.querySelector('.chotacontainer').onclick = function () {
-    document.querySelector('.chotacontainer').innerHTML = "";
-    document.querySelector('.chotacontainer').style.color = "white";
+  document.querySelector('.chotacontainer').innerHTML = "";
+  document.querySelector('.chotacontainer').style.color = "white";
 }
 
 document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('checkbox')) {
-        const checkbox = event.target;
-        const x = checkbox.id.split('-')[1];
-        const element = document.getElementById(`task-${x}`);
-        element.classList.toggle('thicker-strikethrough');
-    }
+  if (event.target.classList.contains('checkbox')) {
+    const checkbox = event.target;
+    const x = checkbox.id.split('-')[1];
+    const element = document.getElementById(`task-${x}`);
+    element.classList.toggle('thicker-strikethrough');
+    checkbox.style.color = checkbox.style.color==="white" ? "#202124":"white" ;
+    element.style.color = checkbox.style.color==="white" ? "#1bff0778":"white" ;
+  }
 
 });
